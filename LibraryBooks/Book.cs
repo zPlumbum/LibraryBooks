f387580a3fbc19cbee51eb8252d6669afd6a4524
Александр Кикоть <academic77@yandex.ru>
2020-01-04 16:21:44 +0400

    Создан класс "Book" с полями для ввода данных и проверкой

---
+++ LibraryBooks/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LibraryBooks
+{
+    class Book
+    {
+        public string bookName { get; }
+        public DateTime publicationYear { get; }
+        public string Author { get; }
+
+        public Book(string name, DateTime date, string author)
+        {
+            #region Chec
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Название книги не может быть пустым.", nameof(name));
+            }
+
+            if (date < DateTime.Parse("01.01.0001") && date > DateTime.Now)
+            {
+                throw new ArgumentException("Дата публикации не может быть раньше 00.00.0000 или позже сегодняшнего дня.", nameof(date));
+            }
+
+            if (string.IsNullOrWhiteSpace(author))
+            {
+                throw new ArgumentNullException("Имя автора не может быть пустым.");
+            }
+            #endregion
+            bookName = name;
+            publicationYear = date;
+            Author = author;
+        }
+
+        public override string ToString()
+        {
+            return bookName;
+        }
+    }
+}
---
+++ LibraryBooks/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Hey, Man!", DateTime.Parse("02.07.2000"), "Kikot");
+            Console.WriteLine(book1.bookName);
+            Console.WriteLine(book1.Author);
+            Console.WriteLine(book1.publicationYear);
+            Console.ReadKey();
+        }
+    }
+}
